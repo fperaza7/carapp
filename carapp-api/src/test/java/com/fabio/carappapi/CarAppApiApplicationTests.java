5c3e0739dabb9f61e784389d3163e6d1062c6697
Fabio Peraza <fabio.peraza@hotmail.com>
2025-10-25 19:12:21 -0500

    chore(init): initial project structure

---
+++ carapp-api/src/test/java/com/fabio/carappapi/CarAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.carappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
